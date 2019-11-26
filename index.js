df2193e3f230a2f4919fe927f6feb4732f5cdcca
hs-yonekura <y-yonekura@science.co.jp>
2019-11-26 11:45:43 +0900

    fix

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const cheerio = require('cheerio');
 
+var imageText = 'Picture';
+var tableText = 'Table'
+
 module.exports = {
+  book: {
+    assets: './assets',
+    css: [
+      'chartnumber.css'
+    ]
+  },
   hooks: {
-    "page": function(page){
-      $ = cheerio.load(page.contents());
-      $('table').each(function(i, table)){
-        console.log($(table).html());
-      }
+    "page": function(page) {
+      var tableCount = 0;
+      var imageCount = 0;
+      if(this.options.pluginsConfig['chartnumber'].image !== undefined)
+        imageText = this.options.pluginsConfig['chartnumber'].image;
+      if(this.options.pluginsConfig['chartnumber'].table !== undefined)
+        tableText = this.options.pluginsConfig['chartnumber'].table;
+      $ = cheerio.load(page.content);
+      $('table').each(function(i, table){
+        tableCount++;
+        $(table).before('<p class="chartnumber">' + tableText + ": " + page.level.replace(/^1\./, '') + '.-' + tableCount);
+      });
+      $('img').each(function(i, img){
+        imageCount++;
+        $(img).before('<p class="chartnumber">' + imageText + ": " + page.level.replace(/^1\./, '') + '.-' + imageCount);
+      });
+      page.content = $.html();
+      return page;
     }
   }
 }
